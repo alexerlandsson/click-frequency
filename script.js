7fbf3d26b3ea5b9115fb441c0293db2cb1cfe7d3
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2015-01-19 21:35:56 +0100

    Update and rename index.html to script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+<!--
+Date:       2015-01-19
+Author:     Alexander Erlandsson
+GitHub:     https://github.com/alexerlandsson
+
+Description:
+This script runs a function if the click frequency is not higher than a specific speed.
+It works by adding a timout to run the funtion after a specific time into an array that
+later gets cleared after every click. If it not gets cleared, the clickTooSlow function
+will trigger.
+-->
+
+var minClickSpeed = 1000;
+var clickTimeouts = [];
+
+function clickFunction() {
+    for (var i = 0; i < clickTimeouts.length; i++) { clearTimeout(clickTimeouts[i]); }
+    document.getElementById('output').innerHTML = "";
+    
+    clickTimeouts.push(setTimeout(clickTooSlow, minClickSpeed));
+}
+
+function clickTooSlow() { 
+    //Some code here...
+}
